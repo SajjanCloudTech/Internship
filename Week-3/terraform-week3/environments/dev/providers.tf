54666c2f9f5cb1ecb5e50731b8d06d5b11e4ccea
SajjanCloudTech <sajjan@cloudtechservice.com>
2025-01-30 14:49:31 +0545

    Added Vault Token from Jenkins Credentials Store

--- Week-3/terraform-week3/environments/dev/providers.tf
+++ Week-3/terraform-week3/environments/dev/providers.tf
@@ -1,4 +1,18 @@
 provider "aws" {
   region  = "us-east-2"
  # profile = "default"
+ access_key = data.vault_kv_secret_v2.aws_creds["access_key"]
+ secret_key = data.vault_kv_secret_v2.aws_creds["secret_key"]
+ token = data.vault_kv_secret_v2.aws_creds["token"]
 }
+
+provider "vault" {
+  address = "http://18.118.27.69:8200/"
+}
+
+# Fetch AWS credentials from Vault
+data "vault_kv_secret_v2" "aws_creds" {
+  mount = "secret"
+  name  = "aws_creds"
+}
+
