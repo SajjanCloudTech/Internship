f1c42e8e755e8ef52c3fabf887eab215addd5e9a
SajjanCloudTech <sajjan@cloudtechservice.com>
2025-02-03 12:06:26 +0545

    Week 4 Nodejs app

---
+++ Week-4/NodejsProject/app1/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5001;
+
+app.get('/', (req, res) => {
+    res.send('Hello from App 1');
+});
+
+app.listen(PORT, () => {
+    console.log(`App 1 running on port ${PORT}`);
+});
---
+++ Week-4/NodejsProject/app2/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5002;
+
+app.get('/', (req, res) => {
+    res.send('Hello from App 2');
+});
+
+app.listen(PORT, () => {
+    console.log(`App 2 running on port ${PORT}`);
+});
