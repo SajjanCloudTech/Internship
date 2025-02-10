c8ca49e44f7331bf5b20af9090911d773bf54721
SajjanCloudTech <sajjan@cloudtechservice.com>
2025-02-10 10:06:14 +0545

    demo

--- Week-4/NodejsProject/app2/server.js
+++ Week-4/NodejsProject/app2/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5002;
 
 app.get('/app2', (req, res) => {
-    res.send('Hello from App 2 terraform & codedeploy demo week-4!!');
+    res.send('Hello from App 2 terraform & codedeploy demo!!');
 });
 
 app.listen(PORT, () => {
