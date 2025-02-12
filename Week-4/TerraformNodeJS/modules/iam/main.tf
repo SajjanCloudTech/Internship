cf5d477e830e66648e1d08f619bda5fcf443bea8
SajjanCloudTech <sajjan@cloudtechservice.com>
2025-02-12 21:09:03 +0545

    Changed build flow from script to codebuild

--- Week-4/TerraformNodeJS/modules/iam/main.tf
+++ Week-4/TerraformNodeJS/modules/iam/main.tf
@@ -285,10 +285,10 @@ resource "aws_iam_policy" "codebuild_s3_policy" {
           "s3:ListBucket"
         ]
         Resource = [
-          # "arn:aws:s3:::artifact-bucket-sajjan",    
-          # "arn:aws:s3:::artifact-bucket-sajjan/*",
-          "arn:aws:s3:::artifact-bucket-sajjan-build",
-          "arn:aws:s3:::artifact-bucket-sajjan-build/*"
+          "arn:aws:s3:::artifact-bucket-sajjan",    
+          "arn:aws:s3:::artifact-bucket-sajjan/*",
+          # "arn:aws:s3:::artifact-bucket-sajjan-build",
+          # "arn:aws:s3:::artifact-bucket-sajjan-build/*"
         ]
       }
     ]
