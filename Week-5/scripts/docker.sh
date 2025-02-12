f8a1c479abe12bb5d8caa8dac9d21f7a0b7f5f81
SajjanCloudTech <sajjan@cloudtechservice.com>
2025-02-12 21:34:41 +0545

    Changed build flow from script to codebuild

--- Week-5/scripts/docker.sh
+++ Week-5/scripts/docker.sh
@@ -4,9 +4,15 @@
 set -e
 
 # Define variables
+AWS_REGION="us-east-2"  # Update to match your AWS region
+ECR_REPO_URL="664418970145.dkr.ecr.us-east-2.amazonaws.com/nodejs-todo-app"
 APP_NAME="todoapp"
-APP_DIR="/var/www/app"  # Update this if your app is in a different location
+APP_DIR="/var/www/app"
 PORT=3000
+
+# Log in to AWS ECR
+echo "Logging into AWS ECR..."
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URL
 
 # Change to the application directory
 echo "Navigating to application directory: $APP_DIR"
@@ -20,13 +26,17 @@ if [ "$(docker ps -q -f name=$APP_NAME)" ]; then
 fi
 
 # Remove any existing Docker image
-if [ "$(docker images -q $APP_NAME)" ]; then
+if [ "$(docker images -q $ECR_REPO_URL)" ]; then
     echo "Removing existing image..."
-    docker rmi $APP_NAME
+    docker rmi $ECR_REPO_URL
 fi
+
+# Pull the latest image from ECR
+echo "Pulling the latest image from ECR..."
+docker pull $ECR_REPO_URL:latest
 
 # Run the new container
 echo "Starting new container..."
-docker run -d -p $PORT:3000 --name $APP_NAME $APP_NAME
+docker run -d -p $PORT:3000 --name $APP_NAME $ECR_REPO_URL:latest
 
 echo "Deployment successful!"
